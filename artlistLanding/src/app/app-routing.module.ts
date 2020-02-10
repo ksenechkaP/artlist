2f5f43f26bf54b09e30d09d8eef9883e66f29175
Ksenia Levinas <zzvezda@gmail,com>
2020-02-10 09:14:42 +0200

    basic rout

--- artlistLanding/src/app/app-routing.module.ts
+++ artlistLanding/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegistrationComponent } from './pages/registration/registration.component';
+import { HomeComponent } from './pages/home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'registration', component: RegistrationComponent },
+  { path: '', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
